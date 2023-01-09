c2b973689f874aced7449ded3940bac8e628dffe
Tom Gregor <tgregor@ivsoftware.com>
2023-01-09 07:57:05 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,2 @@
+In your comment you ponder the question **I wonder if I can just solve it by designing WaitingObj.Read()** so let's entertain that notion.
+
---
+++ special-message-in-queue/Program.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+
+namespace special_message_in_queue
+{
+    internal class Program
+    {
+        //  I wonder if I can just solve it by designing WaitingObj.Read()
+        static void Main(string[] args)
+        {
+            Console.Title = "Test Runner";
+            var stopwatch = new Stopwatch();
+            var WaitingObj = new DesignedObservableQueue();
+
+            stopwatch.Start();
+            _ = WaitingObj.SelfTest();
+
+            // Local test method is expecting to
+            // match the predicate in ~6 seconds.
+            if(TestMethod().Wait(TimeSpan.FromSeconds(10)))
+            {
+                Console.WriteLine($"PASSED {stopwatch.Elapsed}");
+            }
+            else
+            {
+                Console.WriteLine($"FAILED {stopwatch.Elapsed}");
+            }
+            // Local method
+            async Task TestMethod()
+            {
+                await WaitingObj.ReadAsync((message) => message.Message == "special");
+            }
+
+            Console.ReadKey();
+        }
+    }
+    class DesignedObservableQueue : Queue<MockMessage>, INotifyCollectionChanged
+    {
+        public new void Enqueue(MockMessage message)
+        {
+            base.Enqueue(message);
+            CollectionChanged?
+                .Invoke(
+                    this,
+                    new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, message));
+        }
+        public new MockMessage Dequeue()
+        {
+            var message = base.Dequeue();
+            CollectionChanged?
+                .Invoke(
+                    this,
+                    new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, message));
+            return message;
+        }
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+        public async Task ReadAsync(Predicate<MockMessage> condition)
+        {
+            var awaiter = new SemaphoreSlim(0, 1);
+            try
+            {
+                CollectionChanged += localOnCollectionChanged;
+                await awaiter.WaitAsync();
+            }
+            finally
+            {
+                awaiter.Release();
+                CollectionChanged -= localOnCollectionChanged;
+            }
+
+            void localOnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+            {
+                switch (e.Action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                        var message = e.NewItems!.Cast<MockMessage>().First();
+                        if(condition(message))
+                        {
+                            Console.WriteLine($"MATCH: {message.Message}");
+                            awaiter.Release();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"NO MATCH: {message.Message}");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public async Task SelfTest()
+        {
+            foreach (
+                var message in new[]
+                {
+                    "occasion",
+                    "twin",
+                    "intention",
+                    "arrow",
+                    "draw",
+                    "forest",
+                    "special",
+                    "please",
+                    "shell",
+                    "momentum",
+                    "coalition",
+                })
+            {
+                Enqueue(new MockMessage { Message = message });
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+    class MockMessage
+    {
+        public string Message { get; set; } = string.Empty;
+    }
+}
